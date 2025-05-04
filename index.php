4d01d68614247edc24f2e055a9adc5804f3b5c91
Neha Majhi <majhineha17@gmail.com>
2025-05-04 15:36:05 +0545

    new change 2

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 echo '<h1>सुन्दर koshi प्रदेश !</h1>';
 echo'<img src="mnr.jpg"width="700">';
-echo'<h3>Hello Namaskar, Swagat xa Tapailai.ajasnkh</h3>';
+echo'<h3>Hello Namaskar, Swagat xa Tapailai.ajasnkhsss</h3>';
 ?>
